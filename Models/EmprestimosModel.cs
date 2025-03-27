fb2713c1a670ae31e6276cfa4729aee156d658c5
Rubenslourenco <alvesrubens926@gmail.com>
2025-03-27 14:13:25 -0300

    feat: add Emprestimos Model

---
+++ Models/EmprestimosModel.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Threading.Tasks;
+
+namespace emprestimos_livros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
