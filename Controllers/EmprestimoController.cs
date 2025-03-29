ef6bc492471265b5a32eb1f38da032242e26f9d4
Rubenslourenco <alvesrubens926@gmail.com>
2025-03-29 17:00:23 -0300

    feat: foi add a msg de erro e a interação com o banco de dados na controller

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -22,5 +22,24 @@ namespace emprestimos_livros.Controllers
             IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
             return View(emprestimos);
         }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimos)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Add(emprestimos);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
--- Models/EmprestimosModel.cs
+++ Models/EmprestimosModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.Intrinsics.X86;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace emprestimos_livros.Models
     public class EmprestimosModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Digite o nome do recebedor")]
         public string Recebedor { get; set; }
+        [Required(ErrorMessage = "Digite o nome do fornecedor")]
         public string Fornecedor { get; set; }
+        [Required(ErrorMessage = "Digite o nome do livro")]
         public string LivroEmprestado { get; set; }
         public DateTime? dataUltimaAtualizacao { get; set; } = DateTime.Now;
 
