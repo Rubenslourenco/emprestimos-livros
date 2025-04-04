bd7f92490ff1b853f5652d914ae1ec3e97bdda17
Rubenslourenco <alvesrubens926@gmail.com>
2025-04-04 20:02:08 -0300

    feat: add Dto

---
+++ Dto/UsuarioRegisterDto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace emprestimos_livros.Dto
+{
+    public class UsuarioRegisterDto
+    {
+
+        [Required(ErrorMessage = "Digite o Nome!!")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Digite o Sobrenome!!")]
+        public string Sobrenome { get; set; }
+        [Required(ErrorMessage = "Digite o email!!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Digite a senha!!")]
+        public string Senha { get; set; }
+        [Required(ErrorMessage = "Digite a Confirmação da Senha!!")]
+        public string ConfirmarSenha { get; set; }
+
+    }
+}
